0642a21eca116c8c94c5f12afc2410b35e608d38
Steven Setiawan Salim <sstevensetiawan@gmail.com>
2023-01-27 18:12:13 +0700

    basic auth

--- main.go
+++ main.go
@@ -26,6 +26,23 @@ var (
 	dbErr error
 )
 
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		uname, pwd, ok := c.Request.BasicAuth()
+		if !ok {
+			c.Writer.Write([]byte("Username atau Password tidak boleh kosong"))
+			c.Abort()
+			return
+		}
+
+		if uname != "admin" && pwd != "admin" {
+			c.Writer.Write([]byte("Username atau Password salah"))
+			c.Abort()
+			return
+		}
+	}
+}
+
 func main() {
 	if errLoadEnv != nil {
 		fmt.Println("Failed load env")
@@ -47,20 +64,21 @@ func main() {
 	router := gin.Default()
 	// customer
 	router.GET("/customer", controllers.GetAllCustomer)
-	router.POST("/customer", controllers.InsertCustomer)
-	router.PUT("/customer/:id", controllers.UpdateCustomer)
-	router.DELETE("/customer/:id", controllers.DeleteCustomer)
+	router.POST("/customer", AuthMiddleware(), controllers.InsertCustomer)
+	router.PUT("/customer/:id", AuthMiddleware(), controllers.UpdateCustomer)
+	router.DELETE("/customer/:id", AuthMiddleware(), controllers.DeleteCustomer)
 
 	// product
 	router.GET("/product", controllers.GetAllProduct)
-	router.POST("/product", controllers.InsertProduct)
-	router.PUT("/product/:id", controllers.UpdateProduct)
-	router.DELETE("/product/:id", controllers.DeleteProduct)
+	router.POST("/product", AuthMiddleware(), controllers.InsertProduct)
+	router.PUT("/product/:id", AuthMiddleware(), controllers.UpdateProduct)
+	router.DELETE("/product/:id", AuthMiddleware(), controllers.DeleteProduct)
 
 	// invoice
 	router.GET("/invoice", controllers.GetAllInvoice)
 	router.GET("/invoice/:invoice_id", controllers.GetInvoice)
-	router.DELETE("/invoice/:invoice_id", controllers.DeleteInvoice)
+	router.POST("invoice/", AuthMiddleware(), controllers.InsertInvoice)
+	router.DELETE("/invoice/:invoice_id", AuthMiddleware(), controllers.DeleteInvoice)
 
 	router.Run("localhost:8080")
 }
